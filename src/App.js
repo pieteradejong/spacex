62ac6564b617581c9ce24bfa2d44883ba840e699
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-06-29 19:37:45 -0400

    add initial app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+        <header className="App-header">
+            <p>
+            SpaceX, the final frontier. Per aspera ad astra.
+            </p>
+        </header>
+        </div>
+    );
+}
