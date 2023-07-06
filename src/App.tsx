a67f3dc5922337daaeada94ea7d7f6bab31f8d90
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-07-06 18:27:19 -0400

    use spacex api to fetch launches. display in list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    const fetchLaunches = async () => {
+      const response = await fetch("https://api.spacexdata.com/v5/launches/upcoming");
+      const data = await response.json();
+      console.log(data);
+      setLaunches(data);
+    }
+    fetchLaunches();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Launches</p>
+      {
+        launches.map((launch: any) => 
+          (
+            <div className="launch" key={launch.id}>
+              <p>{launch.name}</p>
+              <p>{launch.date_utc}</p>
+            </div>
+          )
+        )
+      }
     </>
   )
 }
