ee5f17c5ad1aa600a2c8c1fa327b7ab95df9a2c6
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-07-07 16:00:06 -0400

    put api urls in constants file

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const SPACEX_API_URL_BASE = "https://api.spacexdata.com/v5"
+export const URL_LAUNCHES = `${SPACEX_API_URL_BASE}/launches`
+
+export const URL_LAUNCHES_PAST = `${SPACEX_API_URL_BASE}/launches/past`
+export const URL_LAUNCHES_LATEST = `${SPACEX_API_URL_BASE}/launches/latest`
+export const URL_LAUNCHES_UPCOMING = `${SPACEX_API_URL_BASE}/launches/upcoming`
+export const URL_LAUNCHES_NEXT = `${SPACEX_API_URL_BASE}/launches/next`
+export const URL_LAUNCHES_ALL = `${SPACEX_API_URL_BASE}/launches/all`
