3c8093b13ac034643111ca882f65cd87985ae16f
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-07-23 18:28:23 -0400

    extract url into constants file

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import LaunchComp from './components/LaunchComp';
 import { Launch } from 'types/Launch';
+import { URL_LAUNCHES_UPCOMING } from './constants';
 
 function App() {
   const [launches, setLaunches] = useState<Launch[]>([]);
 
   useEffect(() => {
     const fetchLaunches = async () => {
-      const response = await fetch("https://api.spacexdata.com/v5/launches/upcoming");
+      const response = await fetch(URL_LAUNCHES_UPCOMING);
       const data: Launch[] = await response.json();
       console.log(data);
       setLaunches(data);
